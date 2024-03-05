7681019dc09d92d8686db7540557f3fc1e58d35b
silence <6367950+TaureHorn@users.noreply.github.com>
2024-03-05 13:51:12 +0000

    forgot to save _echo.js buffer >:L

--- app-macros/_echo.js
+++ app-macros/_echo.js
@@ -1,6 +1,9 @@
-const nameSt = "<strong style='color:#28ffff'>"
+const rounds = await new Roll(`1d10`).roll()
 
-const result_html = `<h1>${nameSt}APP HACK:</h1>*_echo</strong></br>Request hacking assistance from the AI housed inside. For the next ${resultSt}`
+const nameSt = "<strong style='color:#28ffff'>"
+const resultSt = "<strong style='color: white'>"
+
+const result_html = `<h1>${nameSt}APP HACK:</h1>*_echo</strong></br>Request hacking assistance from the AI housed inside. For the next ${resultSt}${rounds.total}</strong> rounds all tech/App-related rolls are -2DR`
 
 game.user.hasRole("GAMEMASTER") ?
     ChatMessage.create({
