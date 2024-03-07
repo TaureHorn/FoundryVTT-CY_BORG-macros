cabe3ba02b753f120a09e1431b5a5fc01ab59c46
silence <6367950+TaureHorn@users.noreply.github.com>
2024-03-07 15:19:06 +0000

    changed RandomiseTokenWildcard so it cant randomly pick the current token image

--- misc/RandomiseTokenWildcard.js
+++ misc/RandomiseTokenWildcard.js
@@ -1,16 +1,33 @@
-if (canvas.tokens.controlled.length !== 1) {
+if (game.user.hasRole("GAMEMASTER") && canvas.tokens.controlled.length === 0) {
     ui.notifications.warn(`You need to select a token`)
 }
 
-let actorData = canvas.tokens.controlled[0].document.actor;
+let actorData = [canvas.tokens.controlled[0].document]
+if (canvas.tokens.controlled.length > 1) {
+    const docData = []
+    canvas.tokens.controlled.forEach((token) => {
+        docData.push(token.document)
+    })
+    actorData = docData
+}
 
-let tokenImgArray = await actorData.getTokenImages();
 
-if (tokenImgArray.length < 2) {
-    ui.notifications.warn(`This actor does not have multiple tokens`)
+if (actorData.length === 0 || typeof actorData[0] === 'undefined') {
+    ui.notifications.warn(`You need to select at least one token`)
+
 } else {
-    let imageChoice = Math.floor(Math.random() * tokenImgArray.length);
-    let image = tokenImgArray[imageChoice]
-    await canvas.tokens.controlled[0].document.update({ "img": image })
+    actorData.forEach(async (token) => {
+        let tokenImgArray = [...await token.actor.getTokenImages()]
+
+        if (tokenImgArray.length < 2) {
+            ui.notifications.warn(`Actor "${token.actor.name}" does not have multiple tokens`)
+
+        } else {
+            tokenImgArray.splice(tokenImgArray.indexOf(token.texture.src), 1)
+            let imageChoice = Math.floor(Math.random() * tokenImgArray.length);
+            let image = tokenImgArray[imageChoice]
+            await token.update({ "img": image })
+        }
+    })
 }
 
