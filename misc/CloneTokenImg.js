9932ce93984edab99c68e5846b0458a4d07f4ea1
silence <6367950+TaureHorn@users.noreply.github.com>
2024-03-09 12:30:01 +0000

    added macro to clone token images to other tokens

---
+++ misc/CloneTokenImg.js
@@ -0,0 +1,17 @@
+if (canvas.tokens.controlled.length !== 1) {
+    ui.notifications.warn(`You need to select just one token to clone an image from`)
+}
+
+const src = canvas.tokens.controlled[0].document.texture.img
+const targets = Array.from(game.users.targets)
+
+console.log(src, targets)
+
+if (targets.length < 1) {
+    ui.notifications.warn(`You need to target at least one token to clone an image to`)
+} else {
+    targets.forEach(async (token) => {
+        await token.document.update({ 'texture.img': src })
+    })
+}
+
