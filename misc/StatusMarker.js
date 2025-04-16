00d5ad17fecdd7bd91085f3865b0c1d909b1e77c
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-04-16 15:24:52 +0100

    updated StatusMarker.js to fit with change from CUB to Condition Lab & Triggler && to auto generate options based on conditions rather than them being hard coded

--- misc/StatusMarker.js
+++ misc/StatusMarker.js
@@ -1,73 +1,57 @@
-// REQUIRES MODULE COMBAT-UTILITY-BELT
-if (game.user.hasRole("GAMEMASTER") && canvas.tokens.controlled.length === 0) {
-    ui.notifications.warn(`You need to select a token`)
+
+// OPEN DIALOG TO SELECT A STATUS MARKER TO ADD/REMOVE FROM SELECTED TOKEN(S)
+
+// REQUIRED MODULE: ConditionLab and Triggler
+
+// exit if no condition lab module
+if (!game.modules.get('condition-lab-triggler')?.active) {
+    return ui.notifications.warn(`Macro - ${this.name}: Required module 'Condition Lab & Triggler' inactive. Exiting.`)
 }
 
-let actorData = [canvas.tokens.controlled[0].document]
-if (canvas.tokens.controlled.length > 1) {
-    const docData = []
-    canvas.tokens.controlled.forEach((token) => {
-        docData.push(token.document)
-    })
-    actorData = docData
+// exit if no tokens
+if (canvas.tokens.controlled.length < 1) {
+    return ui.notifications.warn(`Macro - ${this.name}: You need to select at least one token`)
 }
 
+const tokenData = canvas.tokens.controlled.entries()
+const conditions = game.clt.getConditionsMap().entries()
 
-if (actorData.length === 0 || typeof actorData[0] === 'undefined') {
-    ui.notifications.warn(`You need to select at least one token`)
+let form =
+    `<form class="dialog">
+			<label for="condition" name="CONDITION"</label>
+			<select style="width:100%" id="condition" name="condition">
+    `
 
-} else {
+// iterate over conditions and append options to form based on conditions
+for (const [i, condition] of conditions) {
+    form += `<option${i === 0 ? ' selected' : ''} value="${condition.name}">${condition.name}</option>`
+}
 
-    let form =
-        `<form class="dialog"
-            <div clas="form-group">
-                <label for="condition">CONDITION?</label>
-                <select id="condition" name="condition">
-                    <option value="0 Misc">0 Misc</option>
-                    <option value="1 Misc">1 Misc</option>
-                    <option value="2 Misc">2 Misc</option>
-                    <option value="3 Misc">3 Misc</option>
-                    <option value="AIMING">AIMING</option>
-                    <option selected value="COVERED">COVERED</option>
-                    <option value="DEAD">DEAD</option>
-                    <option value="DYING">DYING</option>
-                    <option value="FLASHLIGHT">FLASHLIGHT</option>
-                    <option value="FLYING">FLYING</option>
-                    <option value="HIGH">HIGH</option>
-                    <option value="INFESTED">INFESTED</option>
-                    <option value="INVISIBLE">INVISIBLE</option>
-                    <option value="LOCKED IN">LOCKED IN</option>
-                    <option value="POSSESSED">POSSESSED</option>
-                    <option value="PRONE">PRONE</option>
-                    <option value="RESTRAINED">RESTRAINED</option>
-                    <option value="STONED">STONED</option>
-                    <option value="SURRENDER">SURRENDER</option>
-                    <option value="UNCONSCIOUS">UNCONSCIOUS</option>
-                    <option value="WIRED IN">WIRED IN</option>
-                </select>
-            </div>
-        </form> `
+form += `</select>
+        </form>
+        `
 
-    new Dialog({
-        title: "CONDITION PICKER",
-        content: form,
-        buttons: {
-            submit: { label: "SUBMIT", callback: () => confirmed = true },
-            cancel: { label: "CANCEL", callback: () => confirmed = false }
-        },
-        default: "submit",
+new Dialog({
+    buttons: {
+        submit: { label: 'submit', callback: () => confirmed = true },
+        cancel: { label: 'cancel', callback: () => confirmed = false }
+    },
+    content: form,
+    default: 'submit',
+    title: 'CONDITION PICKER',
 
-        close: html => {
-            (async () => {
-                if (confirmed) {
-                    const condition = html.find('#condition')[0].value
+    close: html => {
+        (async () => {
+            if (confirmed) {
+                const condition = html.find('#condition')[0].value
 
-                    actorData.forEach(async (token) => {
-                        const toggle = await game.cub.hasCondition(condition, token.actor) ? await game.cub.removeCondition(condition, token.actor) : await game.cub.addCondition(condition, token.actor)
-                    })
+                for (const [i, token] of tokenData) {
+                    game.clt.hasCondition(condition, token)
+                        ? game.clt.removeCondition(condition, token)
+                        : game.clt.addCondition(condition, token)
                 }
-            })();
-        }
-    }).render(true)
-}
+            }
+        })();
+    }
+}).render(true)
 
