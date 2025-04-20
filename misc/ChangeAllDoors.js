e96731cd69bc020a92206bcae7e568d8829f19b3
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-04-20 14:56:17 +0100

    added optional args to ChangeAllDoors to target specific scene or to ignore a list of wall ids

--- misc/ChangeAllDoors.js
+++ misc/ChangeAllDoors.js
@@ -1,14 +1,25 @@
 
 // opens dialog to choose door state for all doors in current scene
-// WARN: if doors have sounds associated, this action is REALLY LOUD
+// WARNING: if doors have sounds associated, this action is REALLY LOUD 
+//
+// NOTE: OPTIONAL ARGS 
+//    exemptions Array [string]: a list of id's to ignore
+//    sceneId String: an id string for a scene, defaults to game.scenes.viewed if not provided
 
 if (!game.user.isGM) {
     return ui.notifications.warn(`${this.name}: GM only!`)
 }
 
+let exemptionList = []
+if (typeof exemptions !== 'undefined') {
+    exemptionList = exemptions
+}
+
+let sceneObj = typeof sceneId === 'undefined' ? game.scenes.viewed : game.scenes.get(sceneId)
+
 let form = `<form class="dialog">
-                <label for="door-state">DOORS STATE</label>
-                <select style="width:100%" id="door-state" name="door-state">`
+					<label for="door-state">DOORS STATE</label>
+					<select style="width:100%" id="door-state" name="door-state">`
 
 let i = 0
 for (const state in CONST.WALL_DOOR_STATES) {
@@ -17,7 +28,7 @@ for (const state in CONST.WALL_DOOR_STATES) {
 
 
 form += `</select>
-    </form>`
+		</form>`
 
 const dialog = new Dialog({
     buttons: {
@@ -32,9 +43,9 @@ const dialog = new Dialog({
         (async () => {
             if (confirmed) {
                 const state = parseInt(html.find('#door-state')[0].value)
-                console.log(state)
-                for await (const wall of game.scenes.viewed.walls) {
+                for await (const wall of sceneObj.walls) {
                     if (wall.door) {
+                        if (exemptionList.includes(wall.id)) return
                         await wall.update({ 'ds': state })
                     }
                 }
