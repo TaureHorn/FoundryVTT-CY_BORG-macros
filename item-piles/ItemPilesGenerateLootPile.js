48437f019ffcbdf0f38d868b5ddc6405223a0e3e
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-06-06 18:36:29 +0100

    updated itempiles

--- item-piles/ItemPilesGenerateLootPile.js
+++ item-piles/ItemPilesGenerateLootPile.js
@@ -1,38 +1,43 @@
-// call on an individual object to generate a loot and make a token an Item Pile
-// ARGS Object {actor}, Object {token}
+class TTY extends FormApplication {
 
-if (!game.modules.get('item-piles') {
-	return ui.notifications.warn(`${this.name}: Item Piles modules is not enabled`)
+    static get defaultOptions() {
+        const defaults = super.defaultOptions;
+        const overrides = {
+            closeOnSubmit: false,
+            id: "CONSOLE",
+            popOut: true,
+            minimizable: true,
+            resizable: false,
+            submitOnChange: true,
+            template: "0_CUSTOM/4_EXPORTS/console.hbs",
+            title: "CONSOLE",
+            userId: game.userId
+        }
+        return foundry.utils.mergeObject(defaults, overrides)
+    }
+
+    getData() {
+        return {
+            header: "Alliansen Inc. OMEGA CLASS SECURITY SYSTEM",
+            src: game.items.get("vm5aj6Qkl20jwlgv"),
+            style: {
+                background: "url(0_CUSTOM/2_ASSETS/corp-logos/alliansen_console-bg.jpg)",
+                col: "#ff0055"
+            }
+        }
+    }
+
+    async _updateObject(event, formData) {
+        const src = this.getData().src
+        const messageLog = [...src.system.chatLog]
+        const name = game.user.character ? `${game.user.character.name}: ` : ""
+        const message = `${name}${formData.consoleInputText}`
+        messageLog.push(message)
+        await src.update({ "system.chatLog": messageLog })
+        this.render()
+    }
+
 }
-if (actor.type != 'npc') return
-if (!game.user.isGM) return
-if (typeof token === 'undefined') return
 
-// generate loot based on table
-await game.itempiles.API.rollItemTable("randomLoot", {
-    timesToRoll: "1d3-1",
-    targetActor: actor,
-    removeExistingActorItems: false
-});
+let konsole = new TTY({}).render(true, { height: 700, width: 700 })
 
-// turn token into item pile
-await game.itempiles.API.turnTokensIntoItemPiles(token, {
-	pileSettings : {
-		shareCurrenciesEnabled: false,
-		displayOne: false,
-		type: game.itempiles.pile_types.PILE,
-	}
-})
-
-// generate a list of items from strings in actors sheet
-let inventory = []
-let invalidTypes = ['class', 'feat', 'infestation', 'nanoPower']
-for (const item of game.items) {
-	if (invalidTypes.includes(item.type)) continue
-	if (actor.system.description.includes(item.id)) inventory.push(item)
-}
-if (inventory.length) await game.itempiles.API.addItems(token, inventory)
-
-// generate random amount of currency and give to token actor
-const value = Math.floor(Math.random() * 50)
-await game.itempiles.API.addCurrencies(actor, `${value}C`)
